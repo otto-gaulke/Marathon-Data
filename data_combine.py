a59d91fdc9ef122384f2ef60a40243293330a1d2
PDswishz <gaulkeotto@gmail.com>
2024-05-31 22:30:09 -0500

    added a single file exporter and data adding function to data_combine.py

--- data_combine.py
+++ data_combine.py
@@ -3,7 +3,7 @@ import os
 import data_parser as dp
 
 
-def combine_all():
+def combine_all(export_name):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     df = pd.DataFrame()
@@ -12,11 +12,33 @@ def combine_all():
     files = [x for x in files if x[-4:] == '.tcx']
 
     for file in files:
-        temp = dp.convert(dp.tcx_parse())
+        temp = dp.convert(dp.tcx_parse(os.getcwd() + '\\' + file))
 
         df = pd.concat([df, temp],
                        axis=0)
 
+    df.to_csv(export_name,
+              index=False)
 
 
-combine_all()
+def add_data(file_add, csv_name, export_name):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    df = pd.read_csv(csv_name)
+
+    temp = dp.convert(dp.tcx_parse(file_add))
+
+    df = pd.concat([df, temp],
+                   axis=0)
+
+    df.to_csv(export_name,
+              index=False)
+
+
+def single_file(file, export_name):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    df = dp.convert(dp.tcx_parse(os.getcwd() + '\\' + file))
+
+    df.to_csv(export_name,
+              index=False)
