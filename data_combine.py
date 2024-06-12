01b2ae353c975399f726c2c8b90acf846c518570
Otto Gaulke <gaulkeotto@gmail.com>
2024-06-12 09:08:08 -0500

    cleaned data_combine.py and overhauled master_file.py

--- data_combine.py
+++ data_combine.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import os
+import datetime as dt
 import data_parser as dp
 
 
-def combine_all(export_name):
+def combine_all(exp_name):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     df = pd.DataFrame()
@@ -12,33 +13,101 @@ def combine_all(export_name):
     files = [x for x in files if x[-4:] == '.tcx']
 
     for file in files:
+        print(f'Processing {file}...')
         temp = dp.convert(dp.tcx_parse(os.getcwd() + '\\' + file))
 
         df = pd.concat([df, temp],
                        axis=0)
 
-    df.to_csv(export_name,
+    df.sort_values(by=['Date', 'Time (Seconds)'],
+                   ascending=True,
+                   inplace=True)
+
+    if exp_name in files:
+        os.remove(os.getcwd() + '\\' + exp_name)
+
+    df.to_csv(exp_name,
               index=False)
 
 
-def add_data(file_add, csv_name, export_name):
+def add_data(filename, exp_name, tcx):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    df = pd.read_csv(csv_name)
+    df = pd.read_csv(filename)
 
-    temp = dp.convert(dp.tcx_parse(file_add))
+    temp = dp.convert(dp.tcx_parse(tcx))
 
     df = pd.concat([df, temp],
                    axis=0)
 
-    df.to_csv(export_name,
+    files = os.listdir(os.getcwd())
+
+    if exp_name in files:
+        os.remove(os.getcwd() + '\\' + exp_name)
+
+    df.to_csv(exp_name,
               index=False)
 
 
-def single_file(file, export_name):
+def single_file(exp_name, tcx):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    df = dp.convert(dp.tcx_parse(os.getcwd() + '\\' + file))
+    df = dp.convert(dp.tcx_parse(os.getcwd() + '\\' + tcx))
 
-    df.to_csv(export_name,
+    files = os.listdir(os.getcwd())
+
+    if exp_name in files:
+        os.remove(os.getcwd() + '\\' + exp_name)
+
+    df.to_csv(exp_name,
+              index=False)
+
+
+def data_entry(filename, exp_name, month, day, year,
+               hours, minutes, seconds, pace_hours,
+               pace_minutes, pace_seconds, distance_miles, avg_hr):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    files = os.listdir(os.getcwd())
+
+    df = pd.read_csv(filename)
+
+    date = dt.date(year=year,
+                   month=month,
+                   day=day)
+
+    time = dt.time(hour=hours,
+                   minute=minutes,
+                   second=seconds)
+
+    pace = dt.time(hour=pace_hours,
+                   minute=pace_minutes,
+                   second=pace_seconds)
+
+    seconds += (minutes + hours * 60) * 60
+
+    distance_m = distance_miles * 1609.344
+
+    distance_km = distance_m / 1000
+
+    new = {'Date': [date], 'Time (Seconds)': [seconds],
+           'Time (Hour-Minute-Second)': [time],
+           'Distance (Meters)': [distance_m],
+           'Distance (Kilometers)': [distance_km],
+           'Distance (Miles)': [distance_miles], 'Pace (Minutes / Mile)': [pace],
+           'Heart Rate': [avg_hr]}
+
+    df = pd.concat([df, pd.DataFrame(new)],
+                   ignore_index=True)
+
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    df.sort_values(by=['Date', 'Time (Seconds)'],
+                   ascending=True,
+                   inplace=True)
+
+    if exp_name in files:
+        os.remove(os.getcwd() + '\\' + exp_name)
+
+    df.to_csv(exp_name,
               index=False)
--- master_file.py
+++ master_file.py
@@ -1,128 +1,385 @@
-import sys
-
-import data_combine as dc
 import os
+import time
+import datetime
+import data_combine as dc
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-maps = {'parse all': 'Parse All', 'add data': 'Add Data',
-        'single file': 'Single File', 'report': 'Report'}
 
-print('\nWould you like to: Parse All, Add Data, create a Single File, '
-      'or create a Report')
+def main(first=1):
+    if first == 1:
+        print('\nHello!')
 
-x = 0
+    while True:
+        print('\nWould you like to create a new file, '
+              'edit an existing file, or analyze data?')
 
-answers = ['Parse All', 'Add Data', 'Single File', 'Report',
-           'parse all', 'add data', 'single file', 'report']
+        new_exist_analyze = input('Enter Create, Edit, or Analyze: ')
 
-while x == 0:
-    ans1 = input('\nParse All/Add Data/Single File/Report: ')
+        accept = ['Create', 'Edit', 'Analyze', 'create', 'edit', 'analyze']
+        mapping = {'create': 'Create', 'edit': 'Edit', 'analyze': 'Analyze'}
 
-    if ans1 not in answers:
-        print('\nIncorrect Entry...')
-        print('Please enter: Parse All, Add Data, Single File, or Report')
+        if new_exist_analyze in accept:
+            if new_exist_analyze in mapping.keys():
+                new_exist_analyze = mapping[new_exist_analyze]
+
+            break
+        else:
+            print('\nInvalid entry...')
+
+    if new_exist_analyze == 'Create':
+        create()
+    elif new_exist_analyze == 'Edit':
+        edit()
+    elif new_exist_analyze == 'Analyze':
+        analyze()
+
+    while True:
+        print('\nWould you like to do anything else?')
+
+        end_ans = input('Enter Yes or No: ')
+
+        accept = ['Yes', 'No', 'yes', 'no']
+        mapping = {'yes': 'Yes', 'no': 'No'}
+
+        if end_ans in accept:
+            if end_ans in mapping.keys():
+                end_ans = mapping[end_ans]
+
+            break
+        else:
+            print('\nInvalid entry...')
+
+    if end_ans == 'Yes':
+        main(first=2)
+    elif end_ans == 'No':
+        print('\nGoodbye...')
+
+        time.sleep(2)
+
+
+def file_check(filename, extension):
+    files = [x for x in os.listdir(os.getcwd()) if x[-4:] == extension]
+
+    if filename in files:
+        return True
     else:
-        x = 1
+        return False
 
-x = 0
 
-if ans1 in maps.keys():
-    ans1 = maps[ans1]
+def warning():
+    while True:
+        print('\nWarning: a file by this name already exists. '
+              'Would you like to overwite this file?')
 
-if ans1 == 'Parse All':
-    while x == 0:
-        exp_name = input('\nProvide a export file name ending in .csv: ')
+        ovwr_ans = input('\nEnter Yes or No: ')
 
-        if exp_name[-4:] != '.csv':
-            print('\nIncorrect Entry...')
+        accept = ['Yes', 'No', 'yes', 'no']
+        mapping = {'yes': 'Yes', 'no': 'No'}
+
+        if ovwr_ans in accept:
+            if ovwr_ans in mapping.keys():
+                ovwr_ans = mapping[ovwr_ans]
+
+            if ovwr_ans == 'Yes':
+                return True
+            elif ovwr_ans == 'No':
+                return False
+
+
+def int_check(value, check):
+    try:
+        value = int(value)
+    except ValueError:
+        check = False
+
+    return value, check
+
+
+def create():
+    print('\nCreate')
+
+    while True:
+        print("\nWould you like to parse all .tcx data into a main .csv, "
+              "parse all .tcx data into separate .csv's, "
+              "or create a single file?")
+
+        all_sep_single = input('Enter Main, Separate, or Single: ')
+
+        accept = ['Main', 'Separate', 'Single', 'main', 'separate', 'single']
+        mapping = {'main': 'Main', 'separate': 'Separate', 'single': 'Single'}
+
+        if all_sep_single in accept:
+            if all_sep_single in mapping.keys():
+                all_sep_single = mapping[all_sep_single]
+
+            break
         else:
-            x = 1
-            dc.combine_all(exp_name)
-elif ans1 == 'Add Data':
-    files = os.listdir(os.getcwd())
+            print('\nInvalid entry...')
 
-    while x == 0:
-        tcx_name = input('\nProvide the name of the source data file: ')
+    if all_sep_single == 'Main':
+        print('\nCreate a main file')
 
-        if tcx_name[-4:] != '.tcx':
-            tcx_name += '.tcx'
+        while True:
+            main_name = input('\nEnter the desired '
+                              'name of the main file (ending in .csv): ')
 
-        if tcx_name not in files:
-            print('\nIncorrect Entry...')
+            if main_name[-4:] == '.csv':
+                check = file_check(filename=main_name,
+                                   extension='.csv')
+
+                if check is True:
+                    cont = warning()
+
+                    if cont is True:
+                        break
+                else:
+                    break
+            else:
+                print('\nInvalid entry...')
+
+        print('\nParsing data...\n')
+
+        dc.combine_all(exp_name=main_name)
+    elif all_sep_single == 'Separate':
+        print('\nCreate separate files')
+
+        files = [x for x in os.listdir(os.getcwd()) if x[-4:] == '.tcx']
+
+        print('\nParsing data...\n')
+
+        for file in files:
+            print(file)
+
+            dc.single_file(exp_name=file[:-4] + '.csv',
+                           tcx=file)
+    elif all_sep_single == 'Single':
+        print('\nCreate a single file')
+
+        while True:
+            single_name = input('\nEnter the desired '
+                                'name of the single file (ending in .csv): ')
+
+            if single_name[-4:] == '.csv':
+                check = file_check(filename=single_name,
+                                   extension='.csv')
+
+                if check is True:
+                    cont = warning()
+
+                    if cont is True:
+                        break
+            else:
+                print('\nInvalid entry...')
+
+        while True:
+            tcx = input('\nEnter the name of the '
+                        'source .tcx file (ending in .tcx): ')
+
+            if tcx[-4:] == '.tcx':
+                check = file_check(filename=tcx,
+                                   extension='.tcx')
+
+                if check is True:
+                    break
+                else:
+                    print('\nInvalid entry...')
+            else:
+                print('\nInvalid entry...')
+
+        print('\nParsing data...')
+
+        dc.single_file(exp_name=single_name,
+                       tcx=tcx)
+
+    print('\nDone!')
+
+
+def edit():
+    print('\nEdit')
+
+    while True:
+        csv = input('\nEnter the name of the file you would like to edit '
+                    '(ending in .csv): ')
+
+        if csv[-4:] == '.csv':
+            check = file_check(filename=csv,
+                               extension='.csv')
+
+            if check is True:
+                break
+            else:
+                print('\nInvalid entry...')
         else:
-            x = 1
+            print('\nInvalid entry...')
 
-    x = 0
+    while True:
+        exp = input('\nEnter the desired name '
+                    'of the edited file (ending in .csv): ')
 
-    while x == 0:
-        csv_name = input('\nProvide the name of the .csv: ')
+        if csv[-4:] == '.csv':
+            check = file_check(filename=exp,
+                               extension='.csv')
 
-        if csv_name[-4:] != '.csv':
-            csv_name += '.csv'
+            if check is True:
+                cont = warning()
 
-        if csv_name not in files:
-            print('\nIncorrect Entry...')
+                if cont is True:
+                    break
+            else:
+                print('\nInvalid entry...')
         else:
-            x = 1
+            print('\nInvalid entry...')
 
-    x = 0
+    while True:
+        date_raw = input('\nEnter the date '
+                         'of the entry (MM/DD/YYYY, ex. 02/08/2000): ')
 
-    while x == 0:
-        exp_name = input('\nProvide an export file name: ')
+        check = True
 
-        if exp_name[-4:] != '.csv':
-            exp_name += '.csv'
+        if not (date_raw[2] == '/' and date_raw[5] == '/'):
+            print('\nInvalid date entry...')
+            continue
 
-        x = 1
+        month = date_raw[0:2]
+        day = date_raw[3:5]
+        year = date_raw[6:10]
 
-    x = 0
+        month, check = int_check(value=month,
+                                 check=check)
+        day, check = int_check(value=day,
+                               check=check)
+        year, check = int_check(value=year,
+                                check=check)
 
-    dc.add_data(tcx_name,
-                csv_name,
-                exp_name)
-elif ans1 == 'Single File':
-    files = os.listdir(os.getcwd())
+        if (not (0 <= month <= 12 and 0 <= day <= 31 and 0 < year)
+                or check is False):
+            print('\nInvalid date entry...')
+            continue
 
-    while x == 0:
-        tcx_name = input('\nProvide the name of the source data file: ')
+        try:
+            datetime.date(year=year,
+                          month=month,
+                          day=day)
+        except ValueError:
+            print('\nInvalid date entry...')
+            continue
 
-        if tcx_name[-4:] != '.tcx':
-            tcx_name += '.tcx'
+        if check is True:
+            break
 
-        if tcx_name not in files:
-            print('\nIncorrect Entry...')
-        else:
-            x = 1
+    while True:
+        time_raw = input('\nEnter the duration '
+                         'of the entry (HH:MM:SS, ex. 01:32:23): ')
 
-    x = 0
+        check = True
 
-    while x == 0:
-        exp_name = input('\nProvide an export file name: ')
+        if not (time_raw[2] == ':' and time_raw[5] == ':'):
+            print('\nInvalid date entry...')
+            continue
 
-        if exp_name[-4:] != '.csv':
-            exp_name += '.csv'
+        hours = time_raw[:-6]
+        minutes = time_raw[-5:-3]
+        seconds = time_raw[-2:]
 
-        x = 1
+        hours, check = int_check(value=hours,
+                                 check=check)
+        minutes, check = int_check(value=minutes,
+                                   check=check)
+        seconds, check = int_check(value=seconds,
+                                   check=check)
 
-    x = 0
+        if (not (0 <= hours and 0 <= minutes <= 59 and 0 <= seconds <= 59)
+                or check is False):
+            print('\nInvalid time entry...')
+            continue
 
-    dc.single_file(tcx_name,
-                   exp_name)
+        try:
+            datetime.time(hour=hours,
+                          minute=minutes,
+                          second=seconds)
+        except ValueError:
+            print('\nInvalid time entry...')
+            continue
 
-x = 0
+        if check is True:
+            break
 
-answers = ['Y', 'N', 'y', 'n']
+    while True:
+        pace_raw = input('\nEnter the pace '
+                         'of the entry (HH:MM:SS, ex. 01:32:32): ')
 
-while x == 0:
-    print('\nWould you like to do anything else?')
+        check = True
 
-    complete = input('Y/N: ')
+        if not (pace_raw[2] == ':' and pace_raw[5] == ':'):
+            print('\nInvalid date entry...')
+            continue
 
-    if complete not in answers:
-        print('\nIncorrect Entry...')
-    elif complete == 'Y' or complete == 'y':
-        x = 1
-        os.system('python master_file.py')
-    elif complete == 'N' or complete == 'n':
-        x = 1
+        p_hours = pace_raw[:-6]
+        p_minutes = pace_raw[-5:-3]
+        p_seconds = pace_raw[-2:]
+
+        p_hours, check = int_check(value=p_hours,
+                                   check=check)
+        p_minutes, check = int_check(value=p_minutes,
+                                     check=check)
+        p_seconds, check = int_check(value=p_seconds,
+                                     check=check)
+
+        if not (0 <= p_hours and 0 <= p_minutes <= 59
+                and 0 <= p_seconds <= 59) or check is False:
+            print('\nInvalid pace entry...')
+            continue
+
+        try:
+            datetime.time(hour=p_hours,
+                          minute=p_minutes,
+                          second=p_seconds)
+        except ValueError:
+            print('\nInvalid pace entry...')
+            continue
+
+        if check is True:
+            break
+
+    while True:
+        distance = input('\nEnter the distance (miles) of the entry: ')
+
+        check = True
+
+        distance, check = int_check(value=distance,
+                                    check=check)
+
+        if distance < 0 or check is False:
+            print('\nInvalid distance entry...')
+            continue
+
+        if check is True:
+            break
+
+    while True:
+        hr = input('\nEnter the average heart rate of the entry: ')
+
+        check = True
+
+        hr, check = int_check(value=hr,
+                              check=check)
+
+        if hr < 0 or check is False:
+            print('\nInvalid heart rate entry...')
+            continue
+
+        if check is True:
+            break
+
+    dc.data_entry(filename=csv, exp_name=exp, month=month, day=day, year=year,
+                  hours=hours, minutes=minutes, seconds=seconds,
+                  pace_hours=p_hours, pace_seconds=p_seconds,
+                  pace_minutes=p_minutes, distance_miles=distance, avg_hr=hr)
+
+
+def analyze():
+    print('\nIn dev...')
+
+
+main()
