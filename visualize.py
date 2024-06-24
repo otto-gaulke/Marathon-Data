b9f486190e323a17bb44cedfe56a2cebda808c8b
Otto Gaulke <gaulkeotto@gmail.com>
2024-06-24 09:41:53 -0500

    updated visualize.py

--- visualize.py
+++ visualize.py
@@ -2,8 +2,11 @@ import pandas as pd
 import folium
 import os
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir('..')
+os.chdir(os.getcwd() + '\\csv')
 
 
 def customizable_lineplot(x, y,
@@ -15,7 +18,7 @@ def customizable_lineplot(x, y,
                           xlim=None, ylim=None, legend_label=None,
                           legend_loc='best',
                           figsize=(10, 6), save_path=None):
-    plt.figure(figsize=figsize)
+    # plt.figure(figsize=figsize)
 
     plt.plot(x, y, linestyle=line_style, color=line_color,
              linewidth=line_width,
@@ -45,17 +48,76 @@ def customizable_lineplot(x, y,
         plt.show()
 
 
-def plot_route(df, date=None, start_zoom=15, filename=None):
+def plot_route(df, date=None, start_zoom=15, filename=None, variance_factor=5):
     if date is not None:
         df = df[df['Date'] == date]
 
-    m = folium.Map(location=[df['Latitude'].iloc[0], df['Longitude'].iloc[0]],
-                   zoom_start=start_zoom)
+    m_hr = folium.Map(location=[df['Latitude'].iloc[0],
+                                df['Longitude'].iloc[0]],
+                      zoom_start=start_zoom)
+    m_p = folium.Map(location=[df['Latitude'].iloc[0],
+                               df['Longitude'].iloc[0]],
+                     zoom_start=start_zoom)
+    m_el = folium.Map(location=[df['Latitude'].iloc[0],
+                                df['Longitude'].iloc[0]],
+                      zoom_start=start_zoom)
 
-    folium.PolyLine(df[['Latitude', 'Longitude']].values, color='blue',
-                    weight=5).add_to(m)
+    heart_rates = df['Heart Rate']
+    pace = df['Pace (Numeric)']
+    el = df['Elevation Change (Meters)']
 
-    folium.PolyLine(df[['Latitude', 'Longitude']].values, color='blue',
-                    weight=5).add_to(m)
+    print(f"Heart Rate Range: {heart_rates.min()} - {heart_rates.max()}")
+    print(f"Pace Range: {pace.min()} - {pace.max()}")
+    print(f"Elevation Change Range: {el.min()} - {el.max()}")
 
-    m.save(os.getcwd() + '\\' + filename)
+    norm_hr = plt.Normalize(vmin=heart_rates.min(), vmax=heart_rates.max())
+    pace_mean = pace.mean()
+    el_mean = el.mean()
+
+    norm_pace = plt.Normalize(
+        vmin=pace_mean - variance_factor * (pace_mean - pace.min()),
+        vmax=pace_mean + variance_factor * (pace.max() - pace_mean))
+    norm_el = plt.Normalize(
+        vmin=el_mean - variance_factor * (el_mean - el.min()),
+        vmax=el_mean + variance_factor * (el.max() - el_mean))
+
+    cmap = plt.get_cmap('coolwarm')
+
+    for i in range(len(df) - 1):
+        color = cmap(norm_hr(heart_rates.iloc[i]))
+        color = mcolors.to_hex(color)
+        folium.PolyLine(
+            locations=[(df.iloc[i]['Latitude'], df.iloc[i]['Longitude']),
+                       (df.iloc[i + 1]['Latitude'],
+                        df.iloc[i + 1]['Longitude'])],
+            color=color,
+            weight=5
+        ).add_to(m_hr)
+
+        color = cmap(norm_pace(pace.iloc[i]))
+        color = mcolors.to_hex(color)
+        folium.PolyLine(
+            locations=[(df.iloc[i]['Latitude'], df.iloc[i]['Longitude']),
+                       (df.iloc[i + 1]['Latitude'],
+                        df.iloc[i + 1]['Longitude'])],
+            color=color,
+            weight=5
+        ).add_to(m_p)
+
+        color = cmap(norm_el(el.iloc[i]))
+        color = mcolors.to_hex(color)
+        folium.PolyLine(
+            locations=[(df.iloc[i]['Latitude'], df.iloc[i]['Longitude']),
+                       (df.iloc[i + 1]['Latitude'],
+                        df.iloc[i + 1]['Longitude'])],
+            color=color,
+            weight=5
+        ).add_to(m_el)
+
+    m_hr.save(os.getcwd() + '\\' + 'hr.html')
+    m_p.save(os.getcwd() + '\\' + 'p.html')
+    m_el.save(os.getcwd() + '\\' + 'el.html')
+
+
+df = pd.read_csv('June 1.csv')
+plot_route(df=df, filename='June 1.html')
